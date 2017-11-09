7fea92d738f66339d59ba166861f9de1b73f797a
George Margaritis <gmargaritis@users.noreply.github.com>
2017-11-09 14:18:42 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#FFA792", "#001686", "#97FF92"]);
+var fill = d3.scale.ordinal().range(["#001686", "#FFA792", "#97FF92"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
